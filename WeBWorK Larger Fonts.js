c6cdb5368b51ae934e28c0f7a741871917a44386
Anthony Du <github@anthonydu.com>
2023-03-05 22:29:32 -0800

    Update WeBWorK Larger Fonts.js

--- WeBWorK Larger Fonts.js
+++ WeBWorK Larger Fonts.js
@@ -1,10 +1,15 @@
 // ==UserScript==
 // @name         WeBWorK Larger Fonts
 // @description  Enlarges problem body font size to 20px
-// @author       anthonydu
+// @author       Anthony Du
 // @include      https://webwork.*
-// @grant        none
 // @icon         https://webwork.maa.org/favicon.ico
+// @grant        none
 // ==/UserScript==
 
-document.getElementById("output_problem_body").style.fontSize = "20px";
+(function() {
+    'use strict';
+    document.getElementById("output_problem_body").style.fontSize = "20px";
+    document.getElementById("site-navigation").style.minWidth = "0";
+    document.getElementById("site-navigation").style.maxWidth = "200px";
+})();
