451da86e80caefefceaa652cefcb98b0f89e8162
Anthony Du <github@anthonydu.com>
2023-03-11 19:12:54 -0800

    Added features to Simplify YouTube

--- Simplify YouTube/content.js
+++ Simplify YouTube/content.js
@@ -3,20 +3,21 @@
 ///
 
 document.addEventListener('yt-navigate-finish', () => {
+  /* Any code below will only run on watch pages */
   if (!window.location.href.includes("watch")) return;
 
   /* Forces theater mode */
   document.querySelector("#page-manager > ytd-watch-flexy").setAttribute("theater", "true");
   document.querySelector("#page-manager > ytd-watch-flexy").setAttribute("theater-requested_", "true");
 
-  /* Prevents autoplay */
-  document.querySelector("#movie_player > div.html5-video-container > video").pause();
+  /* Prevents autoplay (default setting: keep autoplay on) */
+  // document.querySelector("#movie_player > div.html5-video-container > video").pause();
 
   /* Turns off autonav and removes toggle */
   var i = window.setInterval(() => {
     const t = document.querySelector("button.ytp-button[data-tooltip-target-id='ytp-autonav-toggle-button']");
     if (t.getAttribute("aria-label") == "Autoplay is off") {
-      t.style.display = "none";
+      t.style.display = "none !important";
       clearInterval(i);
     } else if (t.getAttribute("aria-label") == "Autoplay is on") {
       t.click();
