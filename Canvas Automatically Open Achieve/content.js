12424ae17072fe7c547695c31485379174d517c2
Anthony Du <github@anthonydu.com>
2023-12-07 21:43:53 -0800

    Delete Canvas Automatically Open Achieve directory

--- Canvas Automatically Open Achieve/content.js
+++
@@ -1,9 +0,0 @@
-if (window.location.href.includes("https://canvas.ubc.ca/courses/105567/modules/items/5148502")) {
-  document.querySelector("#tool_form").setAttribute("target", "")
-  document.querySelector("#tool_form > div > div.load_tab > div > button").click();
-}
-
-if (window.location.href.includes("https://lmslink.bfwpub.com/blti_launches/toolsWidget.php")) {
-  document.querySelector("#main > div:nth-child(1) > div > form").setAttribute("target", "");
-  document.querySelector("#main > div:nth-child(1) > div > form").submit();
-} 
